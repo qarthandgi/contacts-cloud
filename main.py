695d64403829f2dff461c967e1ec25950c3ee151
Niles Brandon <nilesbrandon@Niless-MacBook-Air.local>
2017-07-18 18:43:44 -0400

    small change

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import falcon
 class TestResource(object):
 	def on_get(self, req, res):
 		res.status = falcon.HTTP_200
-		res.body = ('This is me, Falcon, serving a resource!')
+		res.body = ('This is me, Falcon, serving a resource HEY ALL!')
 
 app = falcon.API()
 
